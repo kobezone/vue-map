b1e8430107725bad03497ff4c3db8c6caa9eaa3a
wwmin <wwei.min@163.com>
2016-08-26 17:00:52 +0800

    添加bootstrap以及添加arcgis(难点,将require方法变成全局变量传到组件中)

---
+++ src/components/Map.vue
@@ -0,0 +1,39 @@
+<template>
+    <div id="map">
+        <div id="HomeButton"></div>
+        <div id="LocateButton"></div>
+        <div id="XYinfo"></div>
+    </div>
+</template>
+
+<script>
+
+    export default{
+
+        props: [],
+        data(){
+            return {
+                msg: 'hello vue.',
+            }
+        },
+        ready(){
+            dojoRequire(["esri/map", "dojo/domReady!"], function(Map) {
+                var map = new Map("map", {
+                    center: [-118, 34.5],
+                    zoom: 8,
+                    basemap: "topo"
+                });
+            });
+        },
+        methods: {
+
+        },
+        components: {}
+    }
+</script>
+
+<style scoped>
+    div {
+
+    }
+</style>
